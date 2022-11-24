d69c2d19ae6a14bb81b901b854071865cf104035
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-24 20:54:00 +0300

    completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,18 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    # byebug
+    movie = Movie.create(strong_params)
+    render json: movie, status: 201
+    # resue ActiveRecord::RecordInvalid => invalid
+    # render json: {"errors": invalid.record.errors.message}
+    end
+
+private
+ def strong_params
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+
+ end
+
 end
